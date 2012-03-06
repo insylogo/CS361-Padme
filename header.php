06e60111b522208b149a02d0473e06941edab290
unknown <Rob@.(none)>
2012-03-05 17:39:37 -0800

    bunch of stuff added

--- header.php
+++ header.php
@@ -1,32 +1,28 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
-"http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
 <html>
 <head>
 	<link rel="stylesheet" type="text/css" href="css/bootstrap.css" />
 	<link rel="stylesheet" type="text/css" href="css/jquery.weekcalendar.css" />
 	<script type="text/javascript" src="js/jquery-1.4.2.min.js"></script>
-	<script type="text/javascript" src="js/bootstrap.js"></script>
 	<script type="text/javascript" src="js/jquery-ui-1.8rc3.custom.min.js"></script>
 	<script type="text/javascript" src="js/jquery.weekcalendar.js"></script>
-	<script type="text/javascript">
-		$(document).ready(function () { 
-			alert("derp");
-			 $('#calendar').weekCalendar();
-			 
-			  $('#calendar').weekCalendar({
-			    events:[{"id":10182,
-			      "start":"2009-05-03T12:15:00.000+10:00",
-			      "end":"2009-05-03T13:15:00.000+10:00",
-			      "title":"Lunch with Mike"
-			    }, {
-			      "id":10182,
-			      "start":"2009-05-03T14:00:00.000+10:00",
-			      "end":"2009-05-03T15:00:00.000+10:00",
-			      "title":"Dev Meeting"
-			    }]
-			  });
-			  
+<script type="text/javascript">
+$(function() {
+ 
+	$('#calendar').weekCalendar({
+		events:[{"id":10182,
+		  "start":"2012-03-05T12:15:00.000+10:00",
+		  "end":"2012-03-05T13:15:00.000+10:00",
+		  "title":"Lunch with Mike"
+		}, {
+		  "id":10183,
+		  "start":"2012-03-05T14:00:00.000+10:00",
+		  "end":"2012-03-08T15:00:00.000+10:00",
+		  "title":"Dev Meeting"
+		    }]
 		});
+	  
+});
 	</script>
 </head>
 
